e8a9cec36f13355df62aab35a2fdc7c3da2b4c35
narrow-space <hasanimrantri@gmail.com>
2024-04-22 01:03:44 +0600

    first commit

--- controllers/user/userControlers.js
+++ controllers/user/userControlers.js
@@ -129,7 +129,7 @@ exports.Forgotpasssword = async (req, res) => {
 
       ///SEt token and logo in ejs file///
       const data = {
-        passwordresetlink: `https://ecommerce-website-vlto.vercel.app/resetpassword/${userFind.id}/${setuserToken.
+        passwordresetlink: `https://emartly.vercel.app/resetpassword/${userFind.id}/${setuserToken.
           verifytoken}`,
         logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png"
       }
